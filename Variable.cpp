c519baa9ca114c95fe1580d7d78b8537e2784c68
Diego Alcantara <dalcantara7@email.arizona.edu>
2018-10-12 09:54:42 -0700

    variable.cpp and .h written and operator.cpp and .h declared

--- Variable.cpp
+++ Variable.cpp
@@ -1,1 +1,37 @@
 #include <stdio.h>
+#include "Variable.h"
+
+
+Variable::Variable() { //default constructor
+    this->name = "undefined";
+    this->vartype = "undefined";
+    this->bitwidth = 0;
+}
+
+Variable::Variable(std::string name, std::string vartype, int bitwidth) {
+    this->name = name;
+    this->vartype = vartype;
+    this->bitwidth = bitwidth;
+}
+
+//getters
+std::string Variable::getName() const {
+    return this->name;
+}
+std::string Variable::getVarType() const{
+    return this->vartype;
+}
+int Variable::getBitWidth() const{
+    return this->bitwidth;
+}
+
+//setters
+void Variable::setName(std::string name){
+    this->name = name;
+}
+void Variable::setVarType(std::string varType){
+    this->vartype = vartype;
+}
+void Variable::setBitWidth(int bitwidth){
+    this->bitwidth = bitwidth;
+}
