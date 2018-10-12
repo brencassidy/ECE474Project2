98b961bfa4f1cabbda348b470145437007129002
brencass20 <33562885+brencass20@users.noreply.github.com>
2018-10-12 09:21:15 -0700

    Basic Files

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+/*
+Name: Brendan Cassidy, Diego Alcantra, Evan Weiler, Logan Knott
+Project: ECE 474/574 Project 2
+
+Description: 
+	This project is a parser for Behavioral netlists that will output 
+	the top Module verilog file
+*/
+
+int main(int argc, char *argv[]) {
+
+	//Step 1: Read file input line by line
+
+	//Read first keyword to check type
+
+	//Convert to Verilog line of code and export to .v
+	return 0;
+};
