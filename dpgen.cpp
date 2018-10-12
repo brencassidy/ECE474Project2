bc2f5ab520151e9aa3838a1bea1dbc2a469adc16
Evan Weiler <evanweiler@email.arizona.edu>
2018-10-12 09:59:46 -0700

    basic file i/o done

--- dpgen.cpp
+++ dpgen.cpp
@@ -7,19 +7,32 @@ Description:
 	the top Module verilog file
 */
 
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
 int main(int argc, char *argv[]) {
 
 	//Step 1: Read file input line by line
-	std::ifstream iFile(argv[1]);
-	std::string line;
+	ifstream iFile;
+	string line;
+	string fileText;
 
+	iFile.open("C:/Users/evanj/OneDrive/SchoolWork/4Senior/574/ECE474Project2/assignment_2_circuits/474a_circuit1.txt");
 
-	while (!iFile.eof()) {
-		//Read first keyword to check type
-		//line.c_str();
-		printf("%s", line.c_str);
+	if (iFile.is_open()) {
+		while (!iFile.eof()) {
+			//Read first keyword to check type
+			getline(iFile, line);
+			fileText += line + "\n";
+		}
+	} else return -1;
 
-	}
+	cout << fileText;
+	
 	//Convert to Verilog line of code and export to .v
 	return 0;
 };
