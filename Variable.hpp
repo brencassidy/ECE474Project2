cf4c7408590fdcaab12624c78134e351f46302e4
Evan Weiler <evanweiler@email.arizona.edu>
2018-10-23 11:23:59 -0700

    cleaned up code and integrated .cpp into .hpp files

--- Variable.hpp
+++ Variable.hpp
@@ -11,20 +11,43 @@ private:
     int bitwidth;
     
 public:
-    Variable(); //default constructor
-    Variable(std::string name, std::string vartype, int bitwidth);
+	Variable() { //default constructor
+		this->name = "undefined";
+		this->vartype = "undefined";
+		this->bitwidth = 0;
+	}
+
+	Variable(std::string name, std::string vartype, int bitwidth) {
+		this->name = name;
+		this->vartype = vartype;
+		this->bitwidth = bitwidth;
+	}
     
-    //getters
-    std::string getName() const;
-    std::string getVarType() const;
-    int getBitWidth() const;
+	//getters
+	std::string getName() const {
+		return this->name;
+	}
+	std::string getVarType() const {
+		return this->vartype;
+	}
+	int getBitWidth() const {
+		return this->bitwidth;
+	}
     
     //setters
-    void setName(std::string name);
-    void setVarType(std::string varType);
-    void setBitWidth(int bitwidth);
+	void setName(std::string name) {
+		this->name = name;
+	}
+	void setVarType(std::string varType) {
+		this->vartype = varType;
+	}
+	void setBitWidth(int bitwidth) {
+		this->bitwidth = bitwidth;
+	}
 
-	void toString();
+	void toString() {
+		std::cout << "Var Name: " << name << " Var type: " << vartype << " Bit Width: " << bitwidth << std::endl;
+	}
 };
 
 #endif
