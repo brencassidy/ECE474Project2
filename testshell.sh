4062f0787ad4e59cc68f103f983d81e509d434bb
Evan Weiler <evanweiler@email.arizona.edu>
2018-11-02 17:51:13 -0700

    fixed shell script

--- testshell.sh
+++ testshell.sh
@@ -1,31 +1,31 @@
-.\\assignment2.exe .\\assignment_2_circuits\\474a_circuit1.txt .\\output\\circuit1.txt
+./assignment2.exe ./assignment_2_circuits/474a_circuit1.txt ./output/circuit1.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\474a_circuit2.txt .\\output\\circuit2.txt
+./assignment2.exe ./assignment_2_circuits/474a_circuit2.txt ./output/circuit2.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\474a_circuit3.txt .\\output\\circuit3.txt
+./assignment2.exe ./assignment_2_circuits/474a_circuit3.txt ./output/circuit3.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\474a_circuit4.txt .\\output\\circuit4.txt
+./assignment2.exe ./assignment_2_circuits/474a_circuit4.txt ./output/circuit4.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\574a_circuit5.txt .\\output\\circuit5.txt
+./assignment2.exe ./assignment_2_circuits/574a_circuit5.txt ./output/circuit5.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\574a_circuit6.txt .\\output\\circuit6.txt
+./assignment2.exe ./assignment_2_circuits/574a_circuit6.txt ./output/circuit6.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\error1.txt .\\output\\error1.txt
+./assignment2.exe ./assignment_2_circuits/error1.txt ./output/error1.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\error2.txt .\\output\\error2.txt
+./assignment2.exe ./assignment_2_circuits/error2.txt ./output/error2.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\error3.txt .\\output\\error3.txt
+./assignment2.exe ./assignment_2_circuits/error3.txt ./output/error3.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\error4.txt .\\output\\error4.txt
+./assignment2.exe ./assignment_2_circuits/error4.txt ./output/error4.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\mixedcircuit1.txt .\\output\\mixed1.txt
+./assignment2.exe ./assignment_2_circuits/mixedcircuit1.txt ./output/mixed1.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\mixedcircuit2.txt .\\output\\mixed2.txt
+./assignment2.exe ./assignment_2_circuits/mixedcircuit2.txt ./output/mixed2.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\mixedcircuit3.txt .\\output\\mixed3.txt
+./assignment2.exe ./assignment_2_circuits/mixedcircuit3.txt ./output/mixed3.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\ucircuit1.txt .\\output\\unsigned1.txt
+./assignment2.exe ./assignment_2_circuits/ucircuit1.txt ./output/unsigned1.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\ucircuit2.txt .\\output\\unsigned2.txt
+./assignment2.exe ./assignment_2_circuits/ucircuit2.txt ./output/unsigned2.txt
 
-.\\assignment2.exe .\\assignment_2_circuits\\ucircuit3.txt .\\output\\unsigned3.txt
+./assignment2.exe ./assignment_2_circuits/ucircuit3.txt ./output/unsigned3.txt
